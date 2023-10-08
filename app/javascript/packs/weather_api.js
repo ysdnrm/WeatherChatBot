c862a9bb633d5c27183497e7f3b5dd572c4acebf
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-10-08 14:56:24 +0000

    [Update]　天気情報取得の関数

--- app/javascript/packs/weather_api.js
+++ app/javascript/packs/weather_api.js
@@ -14,3 +14,29 @@ async function getWeather(city) {
 }
 
 export { getWeather };
+
+
+
+
+
+// app/javascript/packs/weather_api.js
+
+const apiKey = 'YOUR_API_KEY';  // 取得したAPIキーをセット
+
+async function getWeather(city) {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+    const data = await response.json();
+
+    if (response.ok) {
+      return data.weather[0].description;
+    } else {
+      throw new Error('お天気情報の取得に失敗しました。');
+    }
+  } catch (error) {
+    console.error('エラー:', error);
+    return 'お天気情報を取得できませんでした。';
+  }
+}
+
+export { getWeather };
