68012c9b1678cea4dce4d19c1004a851e7e48a86
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-10-18 14:49:13 +0000

    [Update] レイアウト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ import "channels"
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application."; 
+import "../stylesheets/application.";
 import './chat_bot';
 import '../stylesheets/styles.css';
 
