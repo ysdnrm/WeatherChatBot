4e03b16d38c6732ce9fa888de3619ce3d2bfadd0
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-10-29 16:35:25 +0000

    [Update] 応答をデータベースから取得して返すメソッド追加

--- app/controllers/chat_bots_controller.rb
+++ app/controllers/chat_bots_controller.rb
@@ -1,5 +1,16 @@
 class ChatBotsController < ApplicationController
+  
   def index
     @response = "チャットボット: こんにちは！何か質問があればどんどん聞いてください。"
+  end
+  
+  def get_bot_response
+    user_input = params[:user_input]
+    chat_message = ChatMessage.find_by(question: user_input)
+    if chat_message
+      render plain: chat_message.answer
+    else
+      render plain: "チャットボット: ごめんなさい、よくわかりません。"
+    end
   end
 end
